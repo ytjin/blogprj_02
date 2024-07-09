bddae623f7ef996b36c56d2a43a37f3223336095
ytjin <ytjin@k-dat.com>
2024-07-09 14:09:08 +0900

    modifying models.py, settings.py and create urls.py of blogprj_02 project

--- env/blog/models.py
+++ env/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class BlogAuthor(models.Model):
+    """Model representing an author."""
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["user","bio"]
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular blog author """
+        return reverse('blogs-by-author', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.user.username}'
+    
